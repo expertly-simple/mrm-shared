411c4b794e8a2113494893e2cab0bf3f789e69d5
Doguhan Uluca <duluca@gmail.com>
2019-12-20 17:24:20 -0500

    added setScripts helper

--- helpers.ts
+++ helpers.ts
@@ -1,15 +1,31 @@
 import { json } from "mrm-core";
 
-export function addArrayProperty(fileName: string, propertyName: string, element: string) {
-    let extendsArray = json(fileName).get(propertyName);
-  
-    if (!Array.isArray(extendsArray)) {
-      extendsArray = [extendsArray];
-    }
-  
-    if (!extendsArray.includes(element)) {
-      json(fileName)
-        .set(propertyName, extendsArray.concat(element))
-        .save();
-    }
+export function addArrayProperty(
+  fileName: string,
+  propertyName: string,
+  element: string
+) {
+  let extendsArray = json(fileName).get(propertyName);
+
+  if (!Array.isArray(extendsArray)) {
+    extendsArray = [extendsArray];
   }
+
+  if (!extendsArray.includes(element)) {
+    json(fileName)
+      .set(propertyName, extendsArray.concat(element))
+      .save();
+  }
+}
+
+export function setScript(pkg: PackageJson, name: string, script: string) {
+    pkg
+    .setScript(name, script)
+    .save();
+}
+
+export function setScripts(pkg: PackageJson, scripts: object) {
+    for (let [key, value] of Object.entries(scripts)) {
+        setScript(pkg, key, value)
+    }
+}
