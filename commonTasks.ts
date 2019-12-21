c949665a1f0fcc64a8bb6b178283083f4eef8757
Doguhan Uluca <duluca@gmail.com>
2019-12-20 20:14:37 -0500

    added missing save calls

--- commonTasks.ts
+++ commonTasks.ts
@@ -1,87 +1,86 @@
-import { addArrayProperty, setScripts } from "./helpers";
-import { install, json, lines, packageJson } from "mrm-core";
+import { install, json, lines, packageJson } from 'mrm-core'
+
+import { addArrayProperty, setScripts } from './helpers'
 
 export function configureCommonNpmPackages() {
-  const commonNpm = ["cross-conf-env", "npm-run-all", "dev-norms", "rimraf"];
-  install(commonNpm);
+  const commonNpm = ['cross-conf-env', 'npm-run-all', 'dev-norms', 'rimraf']
+  install(commonNpm)
 }
 
 export function configureImportSort() {
   const importSortPackages = [
-    "import-sort",
-    "import-sort-cli",
-    "import-sort-parser-typescript",
-    "import-sort-style-module"
-  ];
+    'import-sort',
+    'import-sort-cli',
+    'import-sort-parser-typescript',
+    'import-sort-style-module',
+  ]
 
-  install(importSortPackages);
+  install(importSortPackages)
 
-  const pkg = packageJson();
+  const pkg = packageJson()
 
   pkg
-    .set("importSort", {
-      ".ts, .tsx": {
-        parser: "typescript",
-        style: "module",
-        options: {}
-      }
+    .set('importSort', {
+      '.ts, .tsx': {
+        parser: 'typescript',
+        style: 'module',
+        options: {},
+      },
     })
-    .save();
+    .save()
 }
 
 export function configureTsLint() {
-  const tslintPackages = ["tslint", "tslint-etc"];
-  install(tslintPackages);
+  const tslintPackages = ['tslint', 'tslint-etc']
+  install(tslintPackages)
 
-  addArrayProperty("tslint.json", "extends", "tslint-etc");
+  addArrayProperty('tslint.json', 'extends', 'tslint-etc')
 
-  json("tslint.json")
-    .set("rules.no-unused-declaration", true)
-    .set("rules.max-line-length", [false, 90])
-    .set("rules.quotemark", [true, "single", "avoid-escape"])
-    .set("rules.semicolon", [true, "never"])
-    .save();
+  json('tslint.json')
+    .set('rules.no-unused-declaration', true)
+    .set('rules.max-line-length', [false, 90])
+    .set('rules.quotemark', [true, 'single', 'avoid-escape'])
+    .set('rules.semicolon', [true, 'never'])
+    .save()
 }
 
 export function configurePrettier() {
-  const prettierPackages = ["prettier"];
-  install(prettierPackages);
+  const prettierPackages = ['prettier']
+  install(prettierPackages)
 
-  lines(".prettierignore", ["**/*.html"]).save();
+  lines('.prettierignore', ['**/*.html']).save()
 
-  json(".prettierrc")
+  json('.prettierrc')
     .merge({
       tabWidth: 2,
       useTabs: false,
       printWidth: 90,
       semi: false,
       singleQuote: true,
-      trailingComma: "es5",
-      jsxBracketSameLine: true
+      trailingComma: 'es5',
+      jsxBracketSameLine: true,
     })
-    .save();
+    .save()
 
-  const pkg = packageJson();
+  const pkg = packageJson()
 
-  if (pkg.get("devDependencies.tslint")) {
-    const prettierTslintPackages = [
-      "tslint-config-prettier",
-      "tslint-plugin-prettier"
-    ];
-    install(prettierTslintPackages);
+  if (pkg.get('devDependencies.tslint')) {
+    const prettierTslintPackages = ['tslint-config-prettier', 'tslint-plugin-prettier']
+    install(prettierTslintPackages)
 
-    addArrayProperty("tslint.json", "extends", "tslint-config-prettier");
-    addArrayProperty("tslint.json", "extends", "tslint-plugin-prettier");
+    addArrayProperty('tslint.json', 'extends', 'tslint-config-prettier')
+    addArrayProperty('tslint.json', 'extends', 'tslint-plugin-prettier')
 
-    json("tslint.json")
-      .set("rules.prettier", true)
-      .save();
+    json('tslint.json')
+      .set('rules.prettier', true)
+      .save()
   }
 }
 
 export function configurePRTemplate() {
-    if(!lines('pull_request_template.md').exists) {
-      lines('pull_request_template.md').set([
+  if (!lines('pull_request_template.md').exists) {
+    lines('pull_request_template.md')
+      .set([
         '# Feature/Change Description',
         '',
         '_Describe changes made here, link to any issue that is addressed_',
@@ -94,26 +93,27 @@ export function configurePRTemplate() {
         '- [ ] Ran `npm run style:fix` for code style enforcement',
         '- [ ] Ran `npm run lint:fix` for linting',
         '- [ ] Ran `npm audit` to discover vulnerabilities',
-      ]
-      )
-    }
+      ])
+      .save()
   }
-  
-  export function configureInitEnv() {
-    install(['init-dev-env'])
-  
-    if(!lines('example.env').exists()) {
-      lines('example.env').set([
+}
+
+export function configureInitEnv() {
+  install(['init-dev-env'])
+
+  if (!lines('example.env').exists()) {
+    lines('example.env')
+      .set([
         '/* Document required environment variables for .env file here',
         '   Execute npm run init:env to generate a .env file from example',
         '   Ensure .env file is included in .gitignore */',
-        'MY_VAR=defaultValue'
+        'MY_VAR=defaultValue',
       ])
-    }
-    
-    const pkg = packageJson();
-    setScripts(pkg, {
-      "init:env": "init-dev-env generate-dot-env example.env -f"
-    })
+      .save()
   }
-  
+
+  const pkg = packageJson()
+  setScripts(pkg, {
+    'init:env': 'init-dev-env generate-dot-env example.env -f',
+  })
+}
--- helpers.ts
+++ helpers.ts
@@ -1,32 +1,30 @@
-import { json } from "mrm-core";
+import { json } from 'mrm-core'
 
 export function addArrayProperty(
   fileName: string,
   propertyName: string,
   element: string
 ) {
-  let extendsArray = json(fileName).get(propertyName);
+  let extendsArray = json(fileName).get(propertyName)
 
   if (!Array.isArray(extendsArray)) {
-    extendsArray = [extendsArray];
+    extendsArray = [extendsArray]
   }
 
   if (!extendsArray.includes(element)) {
     json(fileName)
       .set(propertyName, extendsArray.concat(element))
-      .save();
+      .save()
   }
 }
 
 export function setScript(pkg: PackageJson, name: string, script: string) {
-    pkg
-    .setScript(name, script)
-    .save();
+  pkg.setScript(name, script).save()
 }
 
 export function setScripts(pkg: PackageJson, scripts: object) {
-    for (let [key, value] of Object.entries(scripts)) {
-        pkg.setScript(key, value)
-    }
-    pkg.save()
+  for (let [key, value] of Object.entries(scripts)) {
+    pkg.setScript(key, value)
+  }
+  pkg.save()
 }
